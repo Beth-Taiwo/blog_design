549609ebd54c6c7b9197137ab39c476d06c714ef
Beth-Tee23 <popoolataiwo22@gmail.com>
2019-08-31 14:32:50 +0000

    first commit

---
+++ js/Contact.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	if( !($("#text").val == "" || $("#text-area").val == "" ) ){
+		
+		$(".btn").on('click',function()){
+			
+		alert("Thanks for your feedback");
+		}
+	}
+});
+
+
---
+++ js/post.js
@@ -0,0 +1,7 @@
+function showText(x){
+	document.querySelector(x).style.display = 'block';
+	//alert('hi');
+}
+function closemodal(X){
+	document.querySelector(X).style.display = 'none';
+}
